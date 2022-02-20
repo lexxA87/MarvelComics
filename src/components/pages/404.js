929391e6e32d44bf385fe3b922bfdf509ef484b3
Lexx <alieksiei.nikonov.87@mail.ru>
2022-02-20 18:42:15 +0300

    18 Added React-Helmet for meta-data

--- src/components/pages/404.js
+++ src/components/pages/404.js
@@ -1,9 +1,14 @@
+import { Helmet } from "react-helmet";
 import ErrorMessage from "../errorMessage/ErrorMessage";
 import { Link } from "react-router-dom";
 
 const Page404 = () => {
   return (
     <div>
+      <Helmet>
+        <meta name="description" content="Page not found" />
+        <title>Page not found</title>
+      </Helmet>
       <ErrorMessage />
       <p style={{ textAlign: "center", fontWeight: "bold", fontSize: "24px" }}>
         Page doesn't exist!
